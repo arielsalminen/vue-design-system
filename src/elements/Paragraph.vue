50a7ef37733b42c20d2d243db21724ef0c9f0516
viljamis <viljami.salminen@gmail.com>
2017-10-25 19:54:36 +0300

    Fix naming

--- src/elements/Paragraph.vue
+++ src/elements/Paragraph.vue
@@ -6,7 +6,7 @@
 
 <script>
   export default {
-    name: 'Text'
+    name: 'Paragraph'
   }
 </script>
 
@@ -27,6 +27,6 @@
 
 <docs>
   ```jsx
-  <text>Design isn’t just about the look and feel. Design is how it works, and we believe the best way to focus on this is to work as close to the end result as possible. That’s why we start all our projects with simple sketches, and quickly transition into design­ing working prototypes in code. This is done by the same designers who started the work, which ensures that our original design intent is carried all the way to the end product.</text>
+  <paragraph>Design isn’t just about the look and feel. Design is how it works, and we believe the best way to focus on this is to work as close to the end result as possible. That’s why we start all our projects with simple sketches, and quickly transition into design­ing working prototypes in code. This is done by the same designers who started the work, which ensures that our original design intent is carried all the way to the end product.</paragraph>
   ```
 </docs>
