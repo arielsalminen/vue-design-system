2cced4cc60f0bf8cabd24035aefdcbdd4d7ebdaa
Viljami Salminen <viljami.salminen@gmail.com>
2017-12-13 17:21:48 +0200

    Fixes production build

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -16,37 +16,37 @@ const webpackConfig = merge(baseWebpackConfig, {
   module: {
     rules: utils.styleLoaders({
       sourceMap: config.build.productionSourceMap,
-      extract: true
-    })
+      extract: true,
+    }),
   },
   devtool: config.build.productionSourceMap ? "#source-map" : false,
   output: {
     path: config.build.assetsRoot,
     filename: utils.assetsPath("js/[name].[chunkhash].js"),
-    chunkFilename: utils.assetsPath("js/[id].[chunkhash].js")
+    chunkFilename: utils.assetsPath("js/[id].[chunkhash].js"),
   },
   plugins: [
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
     new webpack.DefinePlugin({
-      "process.env": env
+      "process.env": env,
     }),
     // UglifyJs do not support ES6+, you can also use babel-minify for better treeshaking: https://github.com/babel/minify
     new webpack.optimize.UglifyJsPlugin({
       compress: {
-        warnings: false
+        warnings: false,
       },
-      sourceMap: true
+      sourceMap: true,
     }),
     // extract css into its own file
     new ExtractTextPlugin({
-      filename: utils.assetsPath("css/[name].[contenthash].css")
+      filename: utils.assetsPath("css/[name].[contenthash].css"),
     }),
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
     new OptimizeCSSPlugin({
       cssProcessorOptions: {
-        safe: true
-      }
+        safe: true,
+      },
     }),
     // generate dist index.html with correct asset hash for caching.
     // you can customize output by editing /index.html
@@ -58,12 +58,12 @@ const webpackConfig = merge(baseWebpackConfig, {
       minify: {
         removeComments: true,
         collapseWhitespace: true,
-        removeAttributeQuotes: true
+        removeAttributeQuotes: true,
         // more options:
         // https://github.com/kangax/html-minifier#options-quick-reference
       },
       // necessary to consistently work with multiple chunks via CommonsChunkPlugin
-      chunksSortMode: "dependency"
+      chunksSortMode: "dependency",
     }),
     // keep module.id stable when vender modules does not change
     new webpack.HashedModuleIdsPlugin(),
@@ -77,23 +77,23 @@ const webpackConfig = merge(baseWebpackConfig, {
           /\.js$/.test(module.resource) &&
           module.resource.indexOf(path.join(__dirname, "../node_modules")) === 0
         );
-      }
+      },
     }),
     // extract webpack runtime and module manifest to its own file in order to
     // prevent vendor hash from being updated whenever app bundle is updated
     new webpack.optimize.CommonsChunkPlugin({
       name: "manifest",
-      chunks: ["vendor"]
+      chunks: ["vendor"],
     }),
     // copy custom static assets
-    new CopyWebpackPlugin([
+    /*new CopyWebpackPlugin([
       {
         from: path.resolve(__dirname, "../static"),
         to: config.build.assetsSubDirectory,
         ignore: [".*"]
       }
-    ])
-  ]
+    ])*/
+  ],
 });
 
 if (config.build.productionGzip) {
@@ -103,18 +103,15 @@ if (config.build.productionGzip) {
     new CompressionWebpackPlugin({
       asset: "[path].gz[query]",
       algorithm: "gzip",
-      test: new RegExp(
-        "\\.(" + config.build.productionGzipExtensions.join("|") + ")$"
-      ),
+      test: new RegExp("\\.(" + config.build.productionGzipExtensions.join("|") + ")$"),
       threshold: 10240,
-      minRatio: 0.8
-    })
+      minRatio: 0.8,
+    }),
   );
 }
 
 if (config.build.bundleAnalyzerReport) {
-  const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
-    .BundleAnalyzerPlugin;
+  const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
   webpackConfig.plugins.push(new BundleAnalyzerPlugin());
 }
 
