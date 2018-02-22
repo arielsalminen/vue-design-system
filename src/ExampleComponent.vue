f6f138354a1ba0ceac8f301a2db2d18200718e84
Viljami Salminen <viljami.salminen@gmail.com>
2018-02-22 14:14:24 +0200

    Adding an example component

---
+++ src/ExampleComponent.vue
@@ -0,0 +1,76 @@
+<template>
+  <component :is="type" :class="['example-component ', variation]">
+    <slot/>
+  </component>
+</template>
+
+<script>
+/**
+ * Components in the system are labelled with status labels that reflect their
+ * state of completion. This is achieved using JSDoc style comments inside the
+ * <script/> block. See example @version below. All available statuses are:
+ *
+ * STATUS:        COLOR:     DESCRIPTION:
+ *
+ * deprecated     Red        Component is deprecated
+ * prototype      Red        Do not implement!
+ * wip            Yellow     Work-in-progress
+ * ready          Green      Ready to be used
+ */
+
+/**
+ * @version `prototype`
+ */
+export default {
+  /**
+   * Component names should always be multi-word, except for root App components.
+   * This prevents conflicts with existing and future HTML elements, since all
+   * HTML elements are a single word. See example below:
+   */
+  name: "ExampleComponent",
+  /**
+   * Prop definitions should be as detailed as possible, specifying at least
+   * type(s). See examples below:
+   */
+  props: {
+    /**
+     * The html element name used for the container
+     */
+    type: {
+      type: String,
+      default: "div",
+    },
+    /**
+     * Style variation to give additional meaning.
+     * `default, disabled, strong, positive, negative, tiny`
+     */
+    variation: {
+      type: String,
+      default: "default",
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+/**
+ * Styles in a top-level App component and in layout components may be global,
+ * but all other components should always be scoped (using either scoped
+ * attribute or class based scoping).
+ */
+.example-component {
+  @include reset;
+  margin: $space-small;
+  @media #{$media-query-medium} {
+    margin: $space-base;
+  }
+}
+</style>
+
+<docs>
+  ```jsx
+  <example-component>
+    Docs section should have an example that is shown in the documentation.
+  </example-component>
+  ```
+</docs>
