92baa1c435d8c7f625204db1b978650ddb1d242c
Viljami Salminen <viljami.salminen@gmail.com>
2018-02-22 14:09:34 +0200

    Fixes spacing component (had broken extra div)

--- docs/components/Spacing.vue
+++ docs/components/Spacing.vue
@@ -1,7 +1,5 @@
 <template>
   <component :is="type" class="spacing">
-    <div v-for="token in designTokens" class="space" />
-
     <div v-for="prop in tokens" class="space" v-if="prop.category === 'space'" :style="{ lineHeight: prop.value, height: prop.value }">
       ${{prop.name.replace(/_/g, "-")}} <span>({{prop.value}})</span>
     </div>
@@ -9,52 +7,52 @@
 </template>
 
 <script>
-import designTokens from "@/assets/tokens/tokens.raw.json";
+import designTokens from "@/assets/tokens/tokens.raw.json"
 
 export default {
   name: "Spacing",
   props: {
     type: {
       type: String,
-      default: "div"
-    }
+      default: "div",
+    },
   },
   methods: {
     orderData: function(data) {
-      let order = _.orderBy(data, "category", "asc");
-      return order;
-    }
+      let order = _.orderBy(data, "category", "asc")
+      return order
+    },
   },
   data() {
     return {
-      tokens: this.orderData(designTokens.props)
-    };
-  }
-};
+      tokens: this.orderData(designTokens.props),
+    }
+  },
+}
 </script>
 
 <style lang="scss" scoped>
-  .spacing {
-    overflow: hidden;
-    width: 100%;
+.spacing {
+  overflow: hidden;
+  width: 100%;
+}
+.space {
+  box-shadow: $box-shadow-tiny-inset;
+  margin-bottom: $space-tiny;
+  font-size: $font-size-tiny;
+  font-family: $font-family-text;
+  color: $color-rich-black;
+  background: #f4f6f8;
+  text-align: center;
+  position: relative;
+  float: left;
+  width: 100%;
+  span {
+    margin-left: 5px;
+    font-style: italic;
+    opacity: 0.5;
   }
-  .space {
-    box-shadow: $box-shadow-tiny-inset;
-    margin-bottom: $space-tiny;
-    font-size: $font-size-tiny;
-    font-family: $font-family-text;
-    color: $color-rich-black;
-    background: #f4f6f8;
-    text-align: center;
-    position: relative;
-    float: left;
-    width: 100%;
-    span {
-      margin-left: 5px;
-      font-style: italic;
-      opacity: 0.5;
-    }
-  }
+}
 </style>
 
 <docs>
