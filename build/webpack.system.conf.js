4056fedb79254a8f22c179a1aa133195a9d04028
Viljami Salminen <viljami.salminen@gmail.com>
2018-02-23 00:19:28 +0200

    Get ready for NPM

--- build/webpack.system.conf.js
+++ build/webpack.system.conf.js
@@ -13,6 +13,10 @@ const UglifyJsPlugin = require("uglifyjs-webpack-plugin")
 
 const env = require("../config/prod.env")
 
+baseWebpackConfig.entry = {
+  system: ["./src/system.js"],
+}
+
 const webpackConfig = merge(baseWebpackConfig, {
   module: {
     rules: utils.styleLoaders({
@@ -24,8 +28,7 @@ const webpackConfig = merge(baseWebpackConfig, {
   devtool: config.build.productionSourceMap ? config.system.devtool : false,
   output: {
     path: config.system.assetsRoot,
-    filename: utils.assetsSystemPath("js/[name].[chunkhash].js"),
-    chunkFilename: utils.assetsSystemPath("js/[id].[chunkhash].js"),
+    filename: utils.assetsSystemPath("[name].js"),
     library: "[name]",
     libraryTarget: "umd",
   },
@@ -45,75 +48,18 @@ const webpackConfig = merge(baseWebpackConfig, {
     }),
     // extract css into its own file
     new ExtractTextPlugin({
-      filename: utils.assetsSystemPath("css/[name].[contenthash].css"),
-      // Setting the following option to `false` will not extract CSS from codesplit chunks.
-      // Their CSS will instead be inserted dynamically with style-loader when the codesplit chunk has been loaded by webpack.
-      // It's currently set to `true` because we are seeing that sourcemaps are included in the codesplit bundle as well when it's `false`,
-      // increasing file size: https://github.com/vuejs-templates/webpack/issues/1110
-      allChunks: true,
+      filename: utils.assetsSystemPath("[name].css"),
+      allChunks: false,
     }),
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
     new OptimizeCSSPlugin({
-      cssProcessorOptions: config.system.productionSourceMap ? { safe: true, map: { inline: false } } : { safe: true },
-    }),
-    // generate dist index.html with correct asset hash for caching.
-    // you can customize output by editing /index.html
-    // see https://github.com/ampedandwired/html-webpack-plugin
-    new HtmlWebpackPlugin({
-      filename: config.system.index,
-      template: "index.html",
-      inject: true,
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true,
-        removeAttributeQuotes: true,
-        // more options:
-        // https://github.com/kangax/html-minifier#options-quick-reference
-      },
-      // necessary to consistently work with multiple chunks via CommonsChunkPlugin
-      chunksSortMode: "dependency",
+      cssProcessorOptions: { safe: true },
     }),
     // keep module.id stable when vendor modules does not change
     new webpack.HashedModuleIdsPlugin(),
     // enable scope hoisting
     new webpack.optimize.ModuleConcatenationPlugin(),
-    // split vendor js into its own file
-    new webpack.optimize.CommonsChunkPlugin({
-      name: "vendor",
-      minChunks(module) {
-        // any required modules inside node_modules are extracted to vendor
-        return (
-          module.resource &&
-          /\.js$/.test(module.resource) &&
-          module.resource.indexOf(path.join(__dirname, "../node_modules")) === 0
-        )
-      },
-    }),
-    // extract webpack runtime and module manifest to its own file in order to
-    // prevent vendor hash from being updated whenever app bundle is updated
-    new webpack.optimize.CommonsChunkPlugin({
-      name: "manifest",
-      minChunks: Infinity,
-    }),
-    // This instance extracts shared chunks from code splitted chunks and bundles them
-    // in a separate chunk, similar to the vendor chunk
-    // see: https://webpack.js.org/plugins/commons-chunk-plugin/#extra-async-commons-chunk
-    new webpack.optimize.CommonsChunkPlugin({
-      name: "app",
-      async: "vendor-async",
-      children: true,
-      minChunks: 3,
-    }),
-
-    // copy custom static assets
-    new CopyWebpackPlugin([
-      {
-        from: path.resolve(__dirname, "../static"),
-        to: config.system.assetsSubDirectory,
-        ignore: [".*"],
-      },
-    ]),
   ],
 })
 
--- src/App.vue
+++ src/App.vue
@@ -11,22 +11,13 @@ export default {
 </script>
 
 <style lang="scss">
-// These styles exist to highlight issues with scoping
-//
-// Elements and Components should have fonts, spacing, etc.
-// defined in them to keep things as modular as possible.
-#app {
-  font: normal normal 100%/normal "Comic Sans MS", "Comic Sans", cursive;
-  height: 100%;
-  color: red;
-}
-* {
-  border-color: red;
-}
 body,
 html {
   height: 100%;
   margin: 0;
   padding: 0;
+}
+#app {
+  height: 100%;
 }
 </style>
