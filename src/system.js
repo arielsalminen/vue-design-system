d22ef4c10f8ada446f49de38f25283f4b4f4b672
Viljami Salminen <viljami.salminen@gmail.com>
2018-02-22 14:15:25 +0200

    Adds design system compiler

---
+++ src/system.js
@@ -0,0 +1,17 @@
+import instance from "@/utils/vueInstance"
+
+// Defines contexts to require
+const contexts = [require.context("@/elements/", true, /\.vue$/), require.context("@/patterns/", true, /\.vue$/)]
+
+const components = []
+contexts.forEach(context => {
+  context.keys().forEach(key => components.push(context(key).default))
+})
+
+export default {
+  install(Vue, options) {
+    components.forEach(c => Vue.component(c.name, c))
+  },
+}
+
+export { instance }
