50e9302fac6409bb0aebfef4274f0854914b1c6b
Viljami Salminen <viljami.salminen@gmail.com>
2018-02-23 11:37:21 +0200

    Run hideprivate only once

---
+++ docs/utils/hidePrivate.js
@@ -0,0 +1,18 @@
+/**
+ * This is “a hack” to hide the private components from the styleguide.
+ * See styleguide.config.js for more information.
+ */
+
+export default {
+  mounted() {
+    if (!document.querySelector(".hide-private")) {
+      const contentHeading = document.getElementById("private-components")
+      const navHeading = document.querySelector("a[href='#private-components']")
+
+      if (contentHeading && navHeading) {
+        contentHeading.parentNode.classList.add("hide-private")
+        navHeading.parentNode.classList.add("hide-private")
+      }
+    }
+  },
+}
