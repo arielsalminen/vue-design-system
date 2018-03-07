0f4531d0b5610cb50cebfacfb036f41682188ed4
Viljami Salminen <viljami.salminen@gmail.com>
2018-03-07 20:04:09 +0200

    Adds route for prototype

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue"
 import Router from "vue-router"
 import Index from "@/templates/Index"
+import Spacing from "@/templates/Spacing"
 import NotFound from "@/templates/NotFound"
 
 Vue.use(Router)
@@ -11,6 +12,11 @@ export default new Router({
       path: "/",
       name: "Index",
       component: Index,
+    },
+    {
+      path: "/spacing/",
+      name: "Spacing",
+      component: Spacing,
     },
     {
       path: "*",
