a415930f9c312d960a33793ccfd7dca4bb33a96e
Viljami Salminen <viljami.salminen@gmail.com>
2018-04-06 15:03:22 +0300

    Rename styleguide to docs

--- src/templates/NotFound.vue
+++ src/templates/NotFound.vue
@@ -2,7 +2,7 @@
   <component :is="type" class="not-found">
     <nav-bar active="NotFound" :navItems="[
       {name: 'Template', component: 'Index', href: '/#/'},
-      {name: 'Style Guide', href: 'http://localhost:6060/'}
+      {name: 'Living Docs', href: 'http://localhost:6060/'}
     ]"/>
     <wrapper>
       <heading level="h1">404 Error</heading>
