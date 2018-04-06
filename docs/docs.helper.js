92bd844fef8cee93c3a835b9cf0a4d1405326c5f
Viljami Salminen <viljami.salminen@gmail.com>
2018-04-06 15:01:30 +0300

    Use consistent naming for docs

--- docs/docs.helper.js
+++ docs/docs.helper.js
@@ -3,7 +3,7 @@
  * You can add more things if/when needed.
  */
 import Vue from "vue"
-import WebFontLoader from "../src/utils/webFontLoader"
+import WebFontLoader from "../src/utils/webFontLoader" // eslint-disable-line no-unused-vars
 import statusLabels from "./utils/statusLabels"
 import activeNav from "./utils/activeNav"
 
