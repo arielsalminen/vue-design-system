9408944ab421abc99d610defc060951f2dc9ce08
Viljami Salminen <viljami.salminen@gmail.com>
2018-04-07 09:27:46 +0300

    Fix issue in fontsize spec

--- test/unit/specs/docs/FontSize.spec.js
+++ test/unit/specs/docs/FontSize.spec.js
@@ -1,5 +1,5 @@
 import Vue from "vue"
-import FontSize from "docs/components/tokens/Fontsize.vue"
+import FontSize from "docs/components/tokens/FontSize.vue"
 
 describe("FontSize.vue", () => {
   it("should render correct contents", () => {
