df805da8ee01fb319823bb122eba443da0ada0c4
Viljami Salminen <viljami.salminen@gmail.com>
2018-09-28 15:29:14 +0300

    Create new input component

--- src/elements/Input.vue
+++ src/elements/Input.vue
@@ -1,29 +1,27 @@
 <template>
-  <component :is="wrapper" class="input">
-    <label v-if="label">{{ label }}</label>
+  <component :is="wrapper" :class="['input', {'input-expand': width === 'expand'}]">
+    <label :for="id" v-if="label">{{ label }}</label>
     <input
       :id="id"
       :disabled="disabled"
       :type="type"
-      :hover="hover"
-      :focus="focus"
       :value="value"
+      :class="state"
       :placeholder="placeholder"
-      :class="['input', {'input-expand': width === 'expand'}]"
       @input="onInput($event.target.value)"
       @focus="onFocus($event.target.value)"
-      />
+    />
   </component>
 </template>
 
 <script>
 /**
- * Form Inputs are used to allow users to provide text input when the expected
- * input is short. Form Input has a range of options and supports several text
- * formats including numbers. For longer input, use the `FormTextarea` element.
+ * Inputs are used to allow users to provide text input when the expected
+ * input is short. Input has a range of options and supports several text
+ * formats including numbers. For longer input, use the `Textarea` element.
  */
 export default {
-  name: "FormInput",
+  name: "Input",
   status: "ready",
   release: "1.0.0",
   props: {
@@ -97,20 +95,15 @@ export default {
       default: false,
     },
     /**
-     * Manually trigger input field’s hover state.
-     * `true, false`
+     * Manually trigger various states of the input.
+     * `hover, active, focus`
      */
-    hover: {
-      type: Boolean,
-      default: false,
-    },
-    /**
-     * Manually trigger input field’s focus state.
-     * `true, false`
-     */
-    focus: {
-      type: Boolean,
-      default: false,
+    state: {
+      type: String,
+      default: null,
+      validator: value => {
+        return value.match(/(hover|active|focus)/)
+      },
     },
   },
   methods: {
@@ -129,31 +122,34 @@ export default {
 $color-placeholder: tint($color-silver, 50%);
 
 .input {
-  @include stack-space($space-small);
-  font-weight: $font-weight-regular;
-  font-family: $font-family-text;
-  font-size: $font-size-base;
-  line-height: $line-height-heading;
+  @include stack-space($space-s);
+  font-weight: $weight-normal;
+  font-family: $font-text;
+  font-size: $size-m;
+  line-height: $line-height-xs;
   width: auto;
-  &.input-expand {
+  &-expand {
     width: 100%;
   }
   label {
+    cursor: pointer;
     display: block;
-    font-size: $font-size-small;
+    font-size: $size-s;
     color: tint($color-rich-black, 20%);
-    @include stack-space($space-x-small);
+    @include stack-space($space-xs);
   }
   input {
     @include reset;
-    @include inset-squish-space($space-small);
+    @include inset-squish-space($space-s);
     transition: all 0.2s ease;
     -webkit-appearance: none;
     appearance: none;
-    font-family: $font-family-text;
+    font-size: $size-m;
+    font-family: $font-text;
     background: $color-white;
-    border-radius: $border-radius-default;
+    border-radius: $radius-default;
     color: set-text-color($color-rich-black, $color-white);
+    width: 100%;
     margin: 0;
     border: 0;
     box-shadow: inset 0 1px 0 0 rgba($color-rich-black, 0.07), 0 0 0 1px tint($color-rich-black, 80%);
@@ -170,20 +166,22 @@ $color-placeholder: tint($color-silver, 50%);
       opacity: 1;
     }
     &:hover,
-    &[hover] {
+    &.hover {
       box-shadow: 0 1px 5px 0 rgba($color-rich-black, 0.07), 0 0 0 1px tint($color-rich-black, 60%);
     }
     &:focus,
-    &[focus] {
+    &.focus {
       transition: box-shadow 0.2s ease;
       box-shadow: inset 0 0 0 1px $color-bleu-de-france, 0 0 0 1px $color-bleu-de-france;
       outline: 0;
     }
     &[disabled] {
-      box-shadow: inset 0 1px 0 0 rgba($color-rich-black, 0.07), 0 0 0 1px tint($color-rich-black, 80%);
+      -webkit-font-smoothing: antialiased;
+      box-shadow: 0 0 0 1px tint($color-rich-black, 80%);
       background: lighten($color-placeholder, 42%);
+      color: tint($color-placeholder, 20%);
       cursor: not-allowed;
-      opacity: 0.5;
+      opacity: 0.7;
     }
   }
 }
@@ -193,10 +191,10 @@ $color-placeholder: tint($color-silver, 50%);
 <docs>
   ```jsx
   <div>
-    <form-input label="Input" placeholder="Write your text" />
-    <form-input label=":hover" hover placeholder="Write your text" />
-    <form-input label=":focus" focus placeholder="Write your text" />
-    <form-input label="[disabled]" disabled placeholder="Disabled input" />
+    <Input label="Default input" placeholder="Write your text" id="input-1" />
+    <Input label=":hover" state="hover" placeholder="Write your text" id="input-2" />
+    <Input label=":focus" state="focus" placeholder="Write your text" id="input-3" />
+    <Input label="[disabled]" disabled value="Write your text" id="input-4" />
   </div>
   ```
 </docs>
