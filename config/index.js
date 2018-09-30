5b8de74b14e536a84d734be4b0c57170589d6cf5
Viljami Salminen <viljami.salminen@gmail.com>
2018-09-30 16:40:13 +0300

    Updates comments

--- config/index.js
+++ config/index.js
@@ -45,12 +45,13 @@ module.exports = {
     assetsSubDirectory: "system",
     assetsPublicPath: "/",
 
-    // The system.js library target, supports all targets
+    // The system.js library target, supports all
     // Webpack libraryTargets (umd, commonjs2, etc...)
+    //
     // In general you probably want this to be “umd”,
     // but for SSR/Nuxt.js you might want to use “commonjs2”
     // to avoid usage of window object.
-    libraryTarget: "commonjs2",
+    libraryTarget: "umd",
 
     // Enable or disable performance hints on build
     performanceHints: false,
