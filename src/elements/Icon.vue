3b89f0c0f060e6861278b6fc7064fd1a73c106e6
Viljami Salminen <viljami.salminen@gmail.com>
2018-10-06 10:51:27 +0300

    Provide defaults for icons since it’s used in docs components

--- src/elements/Icon.vue
+++ src/elements/Icon.vue
@@ -69,6 +69,10 @@ export default {
 </script>
 
 <style lang="scss">
+// This is here just to provide defaults if the original tokens are removed.
+// Can be removed once you’re ready to start defining your own sizes.
+@import "../../docs/docs.tokens.scss";
+
 // We don’t want to use scoped since these styles need to cascade down to SVGs.
 // We also want to be able to style .icon inside buttons etc.
 .icon {
