71791563d32b5d5a4684f17eff029747515ac2d1
Benjamin Schwarze <benjamin.schwarze@mailboxd.de>
2018-11-16 10:47:13 +0100

    add missing postcss-loader (fixes #128)
    This enables postcss-loader in Design System Docs context and thus e.g. makes autoprefixer work.

--- config/docs.config.js
+++ config/docs.config.js
@@ -175,6 +175,7 @@ module.exports = {
           use: [
             "style-loader",
             "css-loader",
+            "postcss-loader",
             "sass-loader",
             {
               loader: "sass-resources-loader",
